321df4bd8bd1ffa0bb83532fe61b78c08e6546e2
Kal Psycho <kalpsycho51@gmail.com>
2025-08-30 15:41:44 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="text-2

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -16,7 +16,7 @@ const projects = [
     link: '#',
   },
   {
-    title: 'SecureAuth Vault',
+    title: 'Ghost Query',
     description: 'An end-to-end encrypted password manager with multi-factor authentication.',
     image: 'https://images.unsplash.com/photo-1699885960867-56d5f5262d38?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHx0eXBlc2NyaXB0fGVufDB8fHx8MTc1NjU2Nzg1MXww&ixlib=rb-4.1.0&q=80&w=1080',
     aiHint: 'cyber security',
